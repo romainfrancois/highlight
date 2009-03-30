03d0c167c0c4ea6fe6d09c98fcedf02e486ca496
Romain François <romain@r-enthusiasts.com>
2009-03-30 08:22:18 +0000

    only use one header file

--- src/if.h
+++
@@ -1,7 +0,0 @@
-#ifndef HIGHLIGHT_IF_H
-#define HIGHLIGHT_IF_H
-
-static void IfPush(void);
-static void ifpop(void); 
-
-#endif
--- src/srcref.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRCREF_H
-#define SRCREF_H
-#include <Rinternals.h>
-#include "yy.h"
-
-static SEXP makeSrcref(YYLTYPE *, SEXP) ;
-static SEXP attachSrcrefs(SEXP, SEXP) ;
-
-#endif
