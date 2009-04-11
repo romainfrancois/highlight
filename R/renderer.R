07d910a08bfb267fe47d6022de37dc6cc56754af
Romain François <romain@r-enthusiasts.com>
2009-04-11 18:21:31 +0000

    minor stuff

--- R/renderer.R
+++ R/renderer.R
@@ -16,13 +16,17 @@
 # - generate a header, e.g write <html>, css definitions, <body>
 # - generate a footer, e.g write </body></html>
 
+# {{{ renderer interface
 renderer <- function( translator, formatter, space, newline, header, footer, ... ){
 	structure( list( translator = translator, 
 		formatter = formatter, space = space, newline = newline, 
 		header = header, footer = footer, ... ), 
 		class = "renderer" )
 }
+# }}}
 
+# {{{ renderer implementations
+# {{{ html
 formatter_html <- function( x ){
 	tokens <- attr( x, "tokens" )
 	styles <- attr( x, "styles" )
@@ -41,12 +45,12 @@ newline_html <- function( n ){
 	"<br/>" 
 }
 
-header_html <- function( ... ){
-	"<html><head></head><body>"
+header_html <- function( con ){
+	cat( "<html>\n<head></head>\n<body>\n", file = con )
 }
 
-footer_html <- function( ... ){
-	"</body></html>"
+footer_html <- function( con ){
+	cat( "\n</body>\n</html>\n", file = con )
 }
 
 renderer_html <- function( translator = translator_html, 
@@ -54,16 +58,20 @@ renderer_html <- function( translator = translator_html,
 	header = header_html, footer = footer_html, ... ){
 	renderer( translator, formatter, space, newline, header, footer, ... )
 }
+# }}}
 
-renderer.latex <- function( x, ...){
+# {{{ latex 
+renderer_latex <- function( ...){
 	.NotYetImplemented( ) 
 }
+# }}}
 
-renderer.ansi <- function( x, ...){
+# {{{ ansi
+renderer_ansi <- function( ...){
 	.NotYetImplemented( ) 
 }
+# }}}
+# }}}
 
-renderer.rtf <- function( x, ...){
-	.NotYetImplemented( )
-}
+# :tabSize=4:indentSize=4:noTabs=false:folding=explicit:collapseFolds=1:
 
