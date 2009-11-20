2e341662c664a56ea9b4d7a9087b802ae951fc73
Romain François <romain@r-enthusiasts.com>
2009-11-20 08:16:20 +0000

    remove getHighlightedText and always return the formatted text from highlight

--- R/highlight.R
+++ R/highlight.R
@@ -17,10 +17,17 @@ highlight <- function( file, output = stdout(),
 	force( styles )
 	force( renderer )
 	
+	# only terminal symbols matter
 	data   <- subset( attr( parser.output, "data" ), terminal ) 
+	
+	# let the renderer do its thing
 	data$ftokens <- renderer$formatter(
 		tokens = renderer$translator( as.character( data[, "text"] ) ), 
 		styles = styles )
+	
+	# useful to only render a given expression and not all of them.
+	# this is mainly used in the sweave driver
+	# FIXME: maybe the renderer should be applied after the subset
 	if( !is.null( expr ) ){
 		ids <- getChilds( parser.output, expr )
 		data <- data[ data$id %in% ids, , drop = FALSE ]
@@ -29,121 +36,33 @@ highlight <- function( file, output = stdout(),
 		startline <- 1L
 	}
 	
-	if( output != stdout() ){
-		sink( file = output ) 
-		on.exit( sink( ) )
+	# paste everything together in C (for efficiency)
+	highlighted_text <- c( renderer$header(), 
+		.Call( "get_highlighted_text", 
+			data$ftokens, 
+			data$token, 
+			data$line1, 
+			data$line2, 
+			data$col1, 
+			data$col2, 
+			data$byte1, 
+			data$byte2, 
+			startline, 
+			max(data$line2) , 
+			renderer$space(), 
+			renderer$newline(), 
+			if( showPrompts) renderer$formatter( renderer$translator( prompt ) , "prompt" ) else "", 
+			if( showPrompts) renderer$formatter( renderer$translator( continue ) , "prompt" ) else "",
+			initial.spaces = initial.spaces, 
+			PACKAGE = "highlight" ), 
+		renderer$footer() )
+	
+	# maybe write the result to the output connection
+	if( !is.null(output) ){
+		try( writeLines( highlighted_text, output, sep = "" ), silent = TRUE )
 	}
-	
-	# this would be better to pass the connection to C instead of sinking to it
-	# but the R connections are not part of the C API
-	# ... maybe some day
-	.External( "do_render", 
-		header  = renderer$header(), 
-		footer  = renderer$footer(), 
-		newline = renderer$newline(),
-		space   = renderer$space(),
-		tokens  = data$ftokens, 
-		line1   = data$line1 , 
-		line2   = data$line2 , 
-		col1    = data$col1, 
-		col2    = data$col2, 
-		byte1   = data$byte1, 
-		byte2   = data$byte2, 
-		startline = startline, 
-		final   = final.newline,
-		type    = data$token, 
-		prompt  = if( showPrompts) renderer$formatter( renderer$translator( prompt ) , "prompt" ) else "" , 
-		continue = if( showPrompts) renderer$formatter( renderer$translator( continue ) , "prompt" ) else "", 
-		initial.spaces = initial.spaces 
-		)
-	
-	# {{{ the C version does the same but faster, this is retained
-	# here in case we need additional functionality which 
-	# is not doable in C
-	# 
-	# write( renderer$header( ) )
-	# for( i in seq_len(ntoks) ){
-	# 	
-	# 	if( line < line1[i] ){
-	# 		for( j in 1:(line1[i]-line) ){
-	# 			write( renderer$newline( ) )
-	# 		}
-	# 		line <- line1[i]
-	# 		col <- 0
-	# 		byte <- 0
-	# 	}
-	# 	
-	# 	if( byte < byte1[i] ){
-	# 		nspaces <- col1[i] - col
-	# 		for( j in 1:nspaces){
-	# 			write( renderer$space( ) )
-	# 		}
-	# 	}
-	# 	write( ftokens[i] )
-	# 	
-	# 	col <- col2[i]
-	# 	byte <- byte2[i]
-	# 	line <- line2[i]
-	# }
-	# if( final.newline ){
-	# 	write( renderer$newline() )
-	# }
-	# write( renderer$footer( ) )
-	# }}}
-	
-	invisible( NULL )
+	invisible( highlighted_text )
 }
-
-getHighlightedText <- function( file, output = stdout(), 
-	detective = simple_detective, renderer, encoding = "unknown",
-	parser.output = parser( file, encoding = encoding ),
-	styles = detective( parser.output ),
-	expr = NULL, 
-	showPrompts = FALSE, 
-	prompt = getOption( "prompt" ) , 
-	continue = getOption( "continue"), 
-	initial.spaces = TRUE, 
-	... ){
-	   
-	# forcing the arguments in a certain order
-	force( parser.output )
-	force( styles )
-	force( renderer )
-	
-	data   <- subset( attr( parser.output, "data" ), terminal ) 
-	data$ftokens <- renderer$formatter(
-		tokens = renderer$translator( as.character( data[, "text"] ) ), 
-		styles = styles )
-	if( !is.null( expr ) ){
-		ids <- getChilds( parser.output, expr )
-		data <- data[ data$id %in% ids, , drop = FALSE ]
-		startline <- as.integer( data[1, "line1" ] )
-	} else{
-		startline <- 1L
-	}
-	
-	highlighted_text <- .Call( "get_highlighted_text", 
-		data$ftokens, 
-		data$token, 
-		data$line1, 
-		data$line2, 
-		data$col1, 
-		data$col2, 
-		data$byte1, 
-		data$byte2, 
-		startline, 
-		max(data$line2) , 
-		renderer$space(), 
-		renderer$newline(), 
-		if( showPrompts) renderer$formatter( renderer$translator( prompt ) , "prompt" ) else "", 
-		if( showPrompts) renderer$formatter( renderer$translator( prompt ) , "prompt" ) else "",
-		PACKAGE = "highlight" )
-	
-	
-	highlighted_text <- c( renderer$header(), highlighted_text, renderer$footer() )
-	highlighted_text
-}
-
 
 # :tabSize=4:indentSize=4:noTabs=false:folding=explicit:collapseFolds=1:
 
--- src/render.c
+++ src/render.c
@@ -14,116 +14,7 @@
 #define ROXYGEN_COMMENT 291
 
 // 2^13
-#define BUFFER_SIZE 16384
-
-
-void write_sexp( SEXP x ){
-	if( x == R_NilValue || length(x) == 0) return ;
-	int n = length( x ) - 1;
-	int i=0; 
-	for( i=0; i<n; i++){
-		Rprintf( CHAR(STRING_ELT(x,i) ) ) ;
-		Rprintf( "\n" ) ;
-	}
-	Rprintf( CHAR(STRING_ELT(x,i) ) ) ;
-}
-
-SEXP attribute_hidden do_render(SEXP args){
-	args = CDR(args); SEXP header = CAR(args) ;
-	args = CDR(args); SEXP footer = CAR(args) ;
-	
-	args = CDR(args);  
-	const char* newline = CHAR(STRING_ELT(CAR(args),0) ) ;
-	
-	args = CDR(args);  
-	const char* space = CHAR(STRING_ELT(CAR(args),0) ) ;
-	
-	args = CDR(args);  SEXP tokens = CAR(args) ;
-	args = CDR(args);  SEXP line1  = CAR(args) ;
-	args = CDR(args);  SEXP line2  = CAR(args) ;
-	args = CDR(args);  SEXP col1   = CAR(args) ;
-	args = CDR(args);  SEXP col2   = CAR(args) ;
-	args = CDR(args);  SEXP byte1  = CAR(args) ;
-	args = CDR(args);  SEXP byte2  = CAR(args) ;
-	args = CDR(args);  int startline  = INTEGER( CAR(args) )[0] ;
-	args = CDR(args);  Rboolean final = LOGICAL( CAR(args) )[0] ;
-	args = CDR(args);  SEXP token_type  = CAR(args) ;
-	args = CDR(args);  
-	const char* prompt = CHAR(STRING_ELT(CAR(args),0) ) ;
-	args = CDR(args);  
-	const char* continue_prompt = CHAR(STRING_ELT(CAR(args),0) ) ;
-	args = CDR(args);  Rboolean initial_spaces = LOGICAL( CAR(args) )[0] ;
-	
-	int n = length( tokens );
-	int line = startline ;
-	int col = 0; 
-	int byte = 0 ;
-	int i, j ;
-	int nspaces ;
-	
-	int useContinuePrompt = 0;
-	int afterLine ;
-	int noMoreRegularPrompt = 0;
-	
-	write_sexp( header );
-	int initial = 1; 
-	
-	Rprintf( "%s", prompt ) ; 
-	for( i=0; i<n; i++){ 
-		/* move down as many lines as needed */
-		if( line < LINE1(i) ){
-			for( ; line < LINE1(i); line++ ){
-				if( (initial == 0) | ( initial_spaces == TRUE ) ){
-					Rprintf( "%s", newline ) ;
-				}
-				if( ( noMoreRegularPrompt == 1 ) | ( useContinuePrompt == 1 ) ){
-					Rprintf( "%s", continue_prompt ) ;
-					noMoreRegularPrompt = 1; 
-				} else{
-					Rprintf( "%s", prompt ) ;
-				}
-				useContinuePrompt = 1;
-			}
-			line = LINE1(i);
-			col  = 0 ;
-			byte = 0 ;
-			afterLine = 1; 
-		}
-		                 
-		/* move right as many spaces as needed */
-		if( byte < BYTE1(i) ){
-			nspaces = COL1(i) - col ;
-			for( j=0; j<nspaces; j++){
-				if( initial == 0 | initial_spaces == TRUE ){
-					Rprintf( "%s", space ) ;
-				}
-			}
-		}
-		
-		/* write the token */ 
-		if( noMoreRegularPrompt == 0){
-			if( TOKEN_TYPE(i) == COMMENT || TOKEN_TYPE(i) == ROXYGEN_COMMENT ){
-				useContinuePrompt = 0 ;
-			} else{
-				useContinuePrompt = 1 ;
-			}
-			afterLine = 0; 
-		} 
-		Rprintf( "%s", TOKEN(i) ) ;
-		initial = 0; 
-		
-		/* set the current positions */ 
-		col = COL2(i);
-		byte = BYTE2(i);
-		line = LINE2(i);
-	}
-	if( final ){
-		Rprintf( "%s", newline ); 
-	}
-	write_sexp( footer );
-	
-	return R_NilValue ;
-}
+#define BUFFER_SIZE 8192
 
 #define PUSH(s) ptr = pushstring(s, ptr)  
 #define PUSHCHAR(c) ptr = pushchar( c, ptr )
@@ -163,16 +54,23 @@ SEXP get_highlighted_text(
 	SEXP byte1, SEXP byte2, 
 	SEXP startline, SEXP endline, 
 	SEXP space, SEXP newline, 
-	SEXP prompt, SEXP continuePrompt ){
+	SEXP prompt, SEXP continuePrompt, 
+	SEXP initialspaces ){
 	
-	char * buffer = malloc( BUFFER_SIZE * sizeof(char) ) ;
+	// TODO: use a smaller buffer and allow it to grow on demand
+	
+	/* set up the buffer that will be used for each line */
+	int current_buffer_size = BUFFER_SIZE ;
+	char * buffer = malloc( current_buffer_size * sizeof(char) ) ;
 	char * ptr = buffer ;
 	
+	/* grab char* for various constants */
 	const char* newline_ = CHAR(STRING_ELT(newline,0) ) ;
 	const char* space_ = CHAR(STRING_ELT(space,0) ) ;
 	const char* prompt_ = CHAR(STRING_ELT(prompt,0) ) ;
 	const char* continuePrompt_ = CHAR(STRING_ELT(continuePrompt,0) ) ;
 	
+	/* first line, last line */
 	int start = INTEGER(startline)[0] ;
 	int end = INTEGER(endline)[0];
 	
@@ -191,7 +89,7 @@ SEXP get_highlighted_text(
 	
 	int initial = 1; 
 	
-	Rboolean initial_spaces = TRUE ;
+	Rboolean initial_spaces = LOGICAL(initialspaces)[0] ;
 	
 	RESET ;
 	PUSH( prompt_ ) ;
@@ -245,12 +143,125 @@ SEXP get_highlighted_text(
 		col = COL2(i);
 		byte = BYTE2(i);
 		line = LINE2(i);
-		
 	}
-	free(buffer) ;
+	
+	/* set the last if needed */
+	if( col ) SET ;
 	UNPROTECT(1) ; /* res */
 	return( res ) ;
 }
+
+// we don't use this anymore, just use writeLines on the R side
+//
+// void write_sexp( SEXP x ){
+// 	if( x == R_NilValue || length(x) == 0) return ;
+// 	int n = length( x ) - 1;
+// 	int i=0; 
+// 	for( i=0; i<n; i++){
+// 		Rprintf( CHAR(STRING_ELT(x,i) ) ) ;
+// 		Rprintf( "\n" ) ;
+// 	}
+// 	Rprintf( CHAR(STRING_ELT(x,i) ) ) ;
+// }
+// SEXP attribute_hidden do_render(SEXP args){
+// 	args = CDR(args); SEXP header = CAR(args) ;
+// 	args = CDR(args); SEXP footer = CAR(args) ;
+// 	
+// 	args = CDR(args);  
+// 	const char* newline = CHAR(STRING_ELT(CAR(args),0) ) ;
+// 	
+// 	args = CDR(args);  
+// 	const char* space = CHAR(STRING_ELT(CAR(args),0) ) ;
+// 	
+// 	args = CDR(args);  SEXP tokens = CAR(args) ;
+// 	args = CDR(args);  SEXP line1  = CAR(args) ;
+// 	args = CDR(args);  SEXP line2  = CAR(args) ;
+// 	args = CDR(args);  SEXP col1   = CAR(args) ;
+// 	args = CDR(args);  SEXP col2   = CAR(args) ;
+// 	args = CDR(args);  SEXP byte1  = CAR(args) ;
+// 	args = CDR(args);  SEXP byte2  = CAR(args) ;
+// 	args = CDR(args);  int startline  = INTEGER( CAR(args) )[0] ;
+// 	args = CDR(args);  Rboolean final = LOGICAL( CAR(args) )[0] ;
+// 	args = CDR(args);  SEXP token_type  = CAR(args) ;
+// 	args = CDR(args);  
+// 	const char* prompt = CHAR(STRING_ELT(CAR(args),0) ) ;
+// 	args = CDR(args);  
+// 	const char* continue_prompt = CHAR(STRING_ELT(CAR(args),0) ) ;
+// 	args = CDR(args);  Rboolean initial_spaces = LOGICAL( CAR(args) )[0] ;
+// 	
+// 	int n = length( tokens );
+// 	int line = startline ;
+// 	int col = 0; 
+// 	int byte = 0 ;
+// 	int i, j ;
+// 	int nspaces ;
+// 	
+// 	int useContinuePrompt = 0;
+// 	int afterLine ;
+// 	int noMoreRegularPrompt = 0;
+// 	
+// 	write_sexp( header );
+// 	int initial = 1; 
+// 	
+// 	Rprintf( "%s", prompt ) ; 
+// 	for( i=0; i<n; i++){ 
+// 		/* move down as many lines as needed */
+// 		if( line < LINE1(i) ){
+// 			for( ; line < LINE1(i); line++ ){
+// 				if( (initial == 0) | ( initial_spaces == TRUE ) ){
+// 					Rprintf( "%s", newline ) ;
+// 				}
+// 				if( ( noMoreRegularPrompt == 1 ) | ( useContinuePrompt == 1 ) ){
+// 					Rprintf( "%s", continue_prompt ) ;
+// 					noMoreRegularPrompt = 1; 
+// 				} else{
+// 					Rprintf( "%s", prompt ) ;
+// 				}
+// 				useContinuePrompt = 1;
+// 			}
+// 			line = LINE1(i);
+// 			col  = 0 ;
+// 			byte = 0 ;
+// 			afterLine = 1; 
+// 		}
+// 		                 
+// 		/* move right as many spaces as needed */
+// 		if( byte < BYTE1(i) ){
+// 			nspaces = COL1(i) - col ;
+// 			for( j=0; j<nspaces; j++){
+// 				if( initial == 0 | initial_spaces == TRUE ){
+// 					Rprintf( "%s", space ) ;
+// 				}
+// 			}
+// 		}
+// 		
+// 		/* write the token */ 
+// 		if( noMoreRegularPrompt == 0){
+// 			if( TOKEN_TYPE(i) == COMMENT || TOKEN_TYPE(i) == ROXYGEN_COMMENT ){
+// 				useContinuePrompt = 0 ;
+// 			} else{
+// 				useContinuePrompt = 1 ;
+// 			}
+// 			afterLine = 0; 
+// 		} 
+// 		Rprintf( "%s", TOKEN(i) ) ;
+// 		initial = 0; 
+// 		
+// 		/* set the current positions */ 
+// 		col = COL2(i);
+// 		byte = BYTE2(i);
+// 		line = LINE2(i);
+// 	}
+// 	if( final ){
+// 		Rprintf( "%s", newline ); 
+// 	}
+// 	write_sexp( footer );
+// 	
+// 	return R_NilValue ;
+// }
+
+
+
 
 
 
