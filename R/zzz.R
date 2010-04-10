bdd756e3a913498b24e4ffddb3b698601c404040
Romain François <romain@r-enthusiasts.com>
2010-04-10 11:20:43 +0000

    custom httpd handler

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,9 @@
 .onAttach <- function( libname, pkgname ){
 	options( detective = simple_detective )
+	
+	if( exists( ".httpd.handlers.env", tools <- asNamespace("tools") ) ){
+		e <- get( ".httpd.handlers.env", tools )
+		e[["highlight"]] <- highlight.httpd.handler
+	}
 }
 
