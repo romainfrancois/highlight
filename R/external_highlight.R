8b1f50a85b1ef5e73e04a419283ca5a0dfd29ddc
Romain François <romain@r-enthusiasts.com>
2010-06-04 16:05:43 +0000

    using Sys.which

--- R/external_highlight.R
+++ R/external_highlight.R
@@ -2,13 +2,6 @@
 private <- new.env()
 
 .findExternalHighlight <- function(){
-	
-	cmd <- "highlight --version"
-	tryCatch( {
-		system( cmd, intern = TRUE )
-		private[["has_highlight"]] <- TRUE
-	}, error = function(e){
-		private[["has_highlight"]] <- FALSE
-	} )
+	private[["has_highlight"]] <- Sys.which( "highlight" ) != ""
 }
 
