61f935378fb15a5eaf8e3d6dc57d18859e651f7e
Romain Francois <romain@r-enthusiasts.com>
2016-10-08 17:16:47 +0200

    simplify arguments for renderer_html

--- R/renderer_html.R
+++ R/renderer_html.R
@@ -91,50 +91,24 @@ footer_html <- function( document ){
 #' 
 #' @param document logical. Indicates if the renderer should render a full document
 #'                 or simply a \samp{<pre>} section containing the highlighted
-#'                 tokens. This argument is used by the \code{\link{header_html}} and 
-#'                 \code{\link{footer_html}} to build appropriate header and footer.
-#' @param translator  Since the highlighted tokens are wrapped in a \samp{<pre>} tag, 
-#'                    no further translation is needed. 
-#' @param formatter  html formatter. creates \samp{<span>} tags for all tokens.
-#'                   See \code{\link{formatter_html}}
-#' @param space returns a space character
-#' @param newline  returns a newline character
-#' @param header html header. Depending on the \samp{document} argument, this will be a 
-#'               function building a the beginning of a 
-#'               complete html document (starting with \samp{<html>}) including 
-#'               css definitions or simply a function returning \samp{<pre>} 
-#'               enabling the renderer to be used to just render the syntax 
-#'               as part of a bigger document.
-#' @param footer html footer. Depending on the \samp{document} argument, this will 
-#'               either close the full document (close the \samp{</html>} tag)
-#'               or simply close the \samp{</pre>} tag.
+#'                 tokens. 
 #' @param stylesheet stylesheet to use. This is used by the header when document is TRUE.
 #'                   The content of the stylesheet is copied verbatim into a \samp{<style>}
 #'                   tag in that case. See \code{\link{getStyleFile}} for details
 #'                   on where the stylesheet can be located
-#' @param x argument to the translator. Returned as is.
-#' @param size font size. ignored
-#' @param \dots Additional arguments. unused.
 #' 
-#' @return  A renderer capable suitable for the \samp{renderer} argument
-#'          of \code{\link{highlight}} 
+#' @return  A renderer capable suitable for the \samp{renderer} argument of \code{\link{highlight}} 
 #' @seealso 	\code{\link{renderer}} for a description of the interface
 #' 	this renderer is implementing. 
 #' 	
-#' 	\code{\link{highlight}} takes a renderer argument to which it 
-#' 	delegates rendering.
+#' 	\code{\link{highlight}} takes a renderer argument to which it delegates rendering.
 #' @export
-renderer_html <- function( document = TRUE, 
-                           translator = translator_html, formatter = formatter_html, 
-                           space = space_html, newline = newline_html, 
-                           header = header_html( document, stylesheet ) , 
-                           footer = footer_html( document ) ,  
-                           stylesheet = "default", 
-                           ... ){
+renderer_html <- function( document = TRUE, stylesheet = "default" ){
   
-  renderer( translator = translator, formatter = formatter, 
-            space = space, newline = newline, 
-            header = header, footer = footer, 
-            stylesheet= stylesheet, 
-            ... )
+  renderer( 
+    translator = translator_html, formatter = formatter_html, 
+    space = space_html, newline = newline_html, 
+    header = header_html( document, stylesheet ), footer = footer_html( document ), 
+    stylesheet= stylesheet
+  )
 }
